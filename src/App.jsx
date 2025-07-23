75cc2703d9f79182471ce39527003ac955793260
Sudipta <hisudipta26@gmail.com>
2025-07-23 22:07:06 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <p>ByeWind - Dashboard</p>
+      </div>
+    </>
+  )
+}
+
+export default App
