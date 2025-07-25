5a6ddb49060138e78d98cfe342a77c000a86d3cc
Sudipta <hisudipta26@gmail.com>
2025-07-25 21:16:41 +0530

    feat: implemented MetricCards and BarChart

--- src/components/Ecommerce/Ecommerce.jsx
+++ src/components/Ecommerce/Ecommerce.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import Metrics from "./Metrics";
 
 const Ecommerce = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const theme = useSelector((state) => state.theme.theme);
 
-export default Ecommerce
+  return (
+    <div className="grid gap-y-5">
+      <h2
+        className={`text-sm font-semibold ${
+          theme ? "text-[#FFFFFF]" : "text-[#1C1C1C]"
+        }`}
+      >
+        eCommerce
+      </h2>
+      <Metrics />
+    </div>
+  );
+};
+
+export default Ecommerce;
---
+++ src/utils/data.js
@@ -0,0 +1,47 @@
+export const statusCard = [
+  {
+    title: "Customers",
+    value: "3,781",
+    increasedBy: "11.01%",
+    bg: "bg-[#E3F5FF]",
+    darkBg: "bg-[#E3F5FF]",
+    textColor: "text-[#1C1C1C]",
+    darkTextColor: "text-[#1C1C1C]",
+  },
+  {
+    title: "Orders",
+    value: "1,219",
+    decreasedBy: "0.03%",
+    bg: "bg-[#F7F9FB]",
+    darkBg: "bg-[#FFFFFF0D]",
+    textColor: "text-[#1C1C1C]",
+    darkTextColor: "text-[#FFFFFF]",
+  },
+  {
+    title: "Revenue",
+    value: "$695",
+    increasedBy: "15.03%",
+    bg: "bg-[#F7F9FB]",
+    darkBg: "bg-[#FFFFFF0D]",
+    textColor: "text-[#1C1C1C]",
+    darkTextColor: "text-[#FFFFFF]",
+  },
+  {
+    title: "Growth",
+    value: "30.1%",
+    increasedBy: "6.08%",
+    bg: "bg-[#E5ECF6]",
+    darkBg: "bg-[#E5ECF6]",
+    textColor: "text-[#1C1C1C]",
+    darkTextColor: "text-[#1C1C1C]",
+  },
+];
+
+export const BarData = [
+  { name: "2023-01-01", actual: 19, projection: 3 },
+  { name: "2023-02-01", actual: 21, projection: 5 },
+  { name: "2023-03-01", actual: 19, projection: 4 },
+  { name: "2023-04-01", actual: 22, projection: 7 },
+  { name: "2023-05-01", actual: 15, projection: 4 },
+  { name: "2023-06-01", actual: 21, projection: 5 },
+];
