17a92bc9984a36973e944a93ad22e0106bdc3e53
Sudipta <hisudipta26@gmail.com>
2025-07-27 00:10:32 +0530

    chore: deleted unnecessary App.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Ecommerce from "./components/Ecommerce/Ecommerce";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
